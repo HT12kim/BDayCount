7c42fb6b1a697e69e4db8b426deba2310becea45
HT12kim <fruciante86@gmail.com>
2025-05-14 22:24:17 +0900

    add kakao adfit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useEffect, useRef, useState } from 'react';
+import Script from 'next/script';
 
 const bearSVG = (
     <svg
@@ -181,6 +182,14 @@ export default function Home() {
                 </form>
                 <div className="w-full min-h-[2.5em]">{message}</div>
             </div>
+            <ins
+                className="kakao_ad_area"
+                style={{ display: 'none' }}
+                data-ad-unit="DAN-4kRmUTKnT8K7zbSH"
+                data-ad-width="320"
+                data-ad-height="100"
+            ></ins>
+            <Script src="//t1.daumcdn.net/kas/static/ba.min.js" strategy="lazyOnload" async />{' '}
             <footer className="footer w-full flex justify-center items-center fixed left-0 bottom-0 z-10 bg-none pointer-events-none">
                 <a
                     href="https://instagram.com/"
