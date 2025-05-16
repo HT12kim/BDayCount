115529027817fbc5ac6d214badb3a6a7dabd859c
HT12kim <fruciante86@gmail.com>
2025-05-17 08:32:22 +0900

    modify href

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,10 +28,10 @@ export default function RootLayout({
         <html lang="ko">
             <head>
                 {/* Favicon */}
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+                <link rel="apple-touch-icon" sizes="180x180" href="https://bdaycnt.netlify.app/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="https://bdaycnt.netlify.app/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="https://bdaycnt.netlify.app/favicon-16x16.png" />
+                <link rel="shortcut icon" href="https://bdaycnt.netlify.app/favicon.ico" type="image/x-icon" />
 
                 {/* Google Analytics */}
                 <Script
