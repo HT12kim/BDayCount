621ec8cf7dc1846f45eacd5e5a81be724d537e34
HT12kim <fruciante86@gmail.com>
2025-05-17 08:25:27 +0900

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,12 @@ export default function RootLayout({
     return (
         <html lang="ko">
             <head>
+                {/* Favicon */}
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+
                 {/* Google Analytics */}
                 <Script
                     src="https://www.googletagmanager.com/gtag/js?id=G-6LY724VDX3"
